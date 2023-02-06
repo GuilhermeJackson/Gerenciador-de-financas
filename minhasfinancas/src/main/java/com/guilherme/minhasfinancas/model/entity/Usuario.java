3c33ca378023cab08bbb7afce0c07046e860e85f
guilherme <guilherme.lamim96@gmail.com>
2023-02-06 15:07:04 -0300

    removendo getters e setters da enity usuário

--- minhasfinancas/src/main/java/com/guilherme/minhasfinancas/model/entity/Usuario.java
+++ minhasfinancas/src/main/java/com/guilherme/minhasfinancas/model/entity/Usuario.java
@@ -25,7 +25,6 @@ import lombok.ToString;
 @Builder
 //@Data 
 public class Usuario {
-	
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,38 +37,5 @@ public class Usuario {
 	private String email;
 	
 	@Column(name = "senha")
-	private String senha;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
+	private String senha;	
 }
